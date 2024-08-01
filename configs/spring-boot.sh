2d556e1917e0f445d6a65dab383330a17a2e7b10
anh0701 <quynhanhphamthi01@gmail.com>
2024-08-01 20:38:39 +0700

    add file configs install-deb, spring-boot

---
+++ configs/spring-boot.sh
@@ -0,0 +1,16 @@
+# install sdkman
+sudo apt install unzip zip curl
+curl -s "https://get.sdkman.io" | bash
+source "/home/username/.sdkman/bin/sdkman-init.sh"
+
+# install spring boot
+sdk install springboot
+
+#install gradle
+sdk install gradle
+
+#install maven
+sdk install maven
+
+# install idea
+
